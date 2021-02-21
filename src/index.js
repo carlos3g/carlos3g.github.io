a45c23cfc35abca0eee5cc5ad80a184a4323abb6
carlos3g <carlosmesquita156@gmail.com>
2021-02-21 08:08:16 -0300

    [REFACTOR] remove almost all

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App';
+import HomePage from './pages/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
