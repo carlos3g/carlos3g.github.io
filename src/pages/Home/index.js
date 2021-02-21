7ba2a0e5810e423d34aaaab479d80750f016a02a
carlos3g <carlosmesquita156@gmail.com>
2021-02-21 12:30:25 -0300

    [FEAT] HomePage version 0.1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 
-import { Container } from './styles';
+import Project from '../../components/Project';
+
+import {
+  Wrapper,
+  AboutMeContainer,
+  Title,
+  Description,
+  ProjectsContainer,
+} from './styles';
+
+const projects = [
+  {
+    tech: 'Javascript',
+    href: 'https://github.com/carlos3g/expo-basic',
+    title: 'expo-basic',
+    description:
+      'expo-basic is a expo template with basic configs and packages already configured.',
+  },
+  {
+    tech: 'Unity',
+    href: 'https://github.com/carlos3g/bugs-evolution',
+    title: 'bugs-evolution',
+    description: 'Simple game created to teach the theory of evolution',
+  },
+];
 
 function App() {
   return (
-    <Container>
-      <h1>Carlos3g</h1>
-    </Container>
+    <Wrapper>
+      <AboutMeContainer>
+        <Title>
+          Hello, I’m
+          <br />
+          Carlos Mesquita.
+        </Title>
+        <Description>
+          Eu sou o Carlos, um jovem apaixonado por aprender coisas novas,
+          tecnologia, programação e algoritmos! atualmente eu sou um estudante
+          no Instituto Federal do Piauí.
+        </Description>
+      </AboutMeContainer>
+
+      <ProjectsContainer>
+        {projects.map((project, index) => (
+          <Project projectInfo={project} key={index} />
+        ))}
+      </ProjectsContainer>
+    </Wrapper>
   );
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,33 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Wrapper = styled.div`
   flex: 1;
+  display: flex;
+  padding: 88px;
+`;
+
+export const AboutMeContainer = styled.div`
+  width: 40%;
+`;
+
+export const Title = styled.h1`
+  font-weight: 700;
+  font-size: 58px;
+  line-height: 64px;
+`;
+
+export const Description = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 28px;
+  color: #949495;
+  margin-top: 32px;
+  width: 408px;
+`;
+
+export const ProjectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 60%;
 `;
