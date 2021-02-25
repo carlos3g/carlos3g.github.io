708634a668e234652f60cfb5d6b36288d74fb1eb
carlos3g <carlosmesquita156@gmail.com>
2021-02-25 18:08:41 -0300

    [FEAT] create a basic NotFound page

---
+++ src/pages/NotFound/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 4rem;
+`;
